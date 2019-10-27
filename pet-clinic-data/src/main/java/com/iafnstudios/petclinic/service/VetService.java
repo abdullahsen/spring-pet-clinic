6f85e99b9cd6120d47fb5138d025d9731b9fc0f0
Abdullah Sen <sen-abdullah@yandex.com>
2019-10-27 05:14:25 +0300

    Created Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/iafnstudios/petclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.iafnstudios.petclinic.service;
+
+import com.iafnstudios.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
