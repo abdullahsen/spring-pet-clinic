50d73e075654035dbb3921960e0a2d948ba74b00
sen-abdullah@yandex.com <sen-abdullah@yandex.com>
2019-11-04 03:49:13 +0300

    Created Speciality Map Service Implementation. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/iafnstudios/petclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package com.iafnstudios.petclinic.service.map;
+
+import com.iafnstudios.petclinic.model.Speciality;
+import com.iafnstudios.petclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality,Long> implements SpecialityService {
+
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long Id) {
+        return super.findById(Id);
+    }
+}
