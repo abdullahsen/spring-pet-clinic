aee51353104d637376d5738b9815a7bbe05829f9
sen-abdullah@yandex.com <sen-abdullah@yandex.com>
2019-11-05 16:46:55 +0300

    Created Visit Map Service. Closes #45

---
+++ pet-clinic-data/src/main/java/com/iafnstudios/petclinic/service/map/VisitServiceMap.java
@@ -0,0 +1,43 @@
+package com.iafnstudios.petclinic.service.map;
+
+import com.iafnstudios.petclinic.model.Visit;
+import com.iafnstudios.petclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
